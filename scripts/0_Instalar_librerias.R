a019a116f130e428ba3897d35460d485258e72f7
Meylin <meylinmoon@gmail.com>
2026-01-27 17:41:29 +0100

    Instalar librerias; descargar archivos base y guardalos

---
+++ scripts/0_Instalar_librerias.R
@@ -0,0 +1,11 @@
+# Instalar los paquete necesarios para lelvar a cabo la descarga y análisis del proyecto
+
+#httr2  --> Descargar datos desde APIs y URLs
+#jsonlite --> Leer y escribir archivos JSON
+#tidyverse --> ncluye dplyr, tidyr, ggplot2
+#lubridate --> Trabajar con fechas y horas
+#sf -->  Trabajar con datos geográficos (GeoJSON, mapas, coordenadas)
+#leaflet --> Crear mapas interactivos en HTML
+
+install.packages(c("httr2", "jsonlite", "tidyverse", 
+                   "lubridate", "sf", "leaflet"))
